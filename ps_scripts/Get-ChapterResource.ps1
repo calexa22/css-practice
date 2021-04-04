a5749d24fda6f7cbbca038a61e73c1a8f10eb70b
Chris Alexander <cea2cool@gmail.com>
2021-04-04 03:01:48 -0500

    Update Get-ChapterResource.ps1
    Remove debug statements from testing

--- ps_scripts/Get-ChapterResource.ps1
+++ ps_scripts/Get-ChapterResource.ps1
@@ -30,20 +30,13 @@ Function Get-ChapterResouse {
     $file = Split-Path $Source -Leaf
 
     $moduleDir = Get-CssDirectory -Path $(Split-Path $PSScriptRoot -Parent) -Index $Module -Assignment $Assignment
-    Write-Host "MODULE ${moduleDir}"
     $chapterDir = Get-CssDirectory -Path $moduleDir -Index $Chapter -Assignment $False
-    Write-Host "CHAPTER ${chapterDir}"
     $resources = Join-Path -Path $chapterDir -ChildPath 'ChapterResources'
     $destination = Join-Path -Path $resources -ChildPath $($file.Substring(0, $file.IndexOf('.')))
 
-    Write-Host "FILE ${file}"
-    Write-Host "RESOURCES ${resources}"
-    
     Expand-Archive -Path $Source -DestinationPath $destination -ErrorAction Ignore
 
     Remove-Item $(Join-Path -Path $destination -ChildPath '__MACOSX') -Recurse
-
-    Write-Host "CHAPTER $(Split-Path $chapterDir -Leaf)"
 
     "Resource => $(Split-Path $destination -Leaf)`n" | Out-File -FilePath $(Join-Path -Path $chapterDir -ChildPath 'notes.txt')
 
